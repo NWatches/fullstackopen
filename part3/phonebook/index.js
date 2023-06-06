123668fb36a7dbdec081fdc2cd6b21ca7508045d
NWatches <nemrnahle@yahoo.com>
2023-06-05 22:35:59 -0700

    removed requestLogger

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose')
 
 app.use(express.static('build'))
 app.use(express.json())
-app.use(requestLogger)
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
