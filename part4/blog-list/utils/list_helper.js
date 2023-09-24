bc752be2b1e0b35654a1e2c2614c26608a0ab200
Nemr Nahle <nemrnahle@Nemrs-Air-2.lan>
2023-09-24 13:06:15 -0700

    checkpoint 4b

---
+++ part4/blog-list/utils/list_helper.js
@@ -0,0 +1,73 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+	return 1 
+}
+
+const totalLikes = (blogs) => {
+	const reducer = (sum, post) => {
+		return sum + post.likes
+	}
+
+	return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+	if (blogs.length == 0) {
+		return null
+	}
+
+	let maxLikes = -1
+	let favorite = null
+
+	for (const blog of blogs) {
+		if (blog.likes > maxLikes) {
+			maxLikes = blog.likes
+			favorite = blog
+		}
+	}
+
+	return {
+		title: favorite.title,
+		author: favorite.author,
+		likes: favorite.likes
+	}
+}
+
+const mostBlogs = (blogs) => {
+	const blogCounts = _.countBy(blogs, 'author')
+	let maxKey = _.max(Object.keys(blogCounts), o => blogCounts[o])
+	
+	let authorMostBlogs = {
+		'author': maxKey,
+		'blogs': blogCounts[maxKey]
+	}
+
+	return authorMostBlogs
+}
+
+const mostLikes = (blogs) => {
+	if (blogs.length == 0) {
+		return null
+	}
+	const authorLikes = _.groupBy(blogs, 'author')
+	console.log('authorLikes: ', authorLikes)
+	const authorMostLikes = _.maxBy(_.keys(authorLikes), author => _.sumBy(authorLikes[author], 'likes'))
+	console.log(authorMostLikes)
+	const mostLikes = _.sumBy(authorLikes[authorMostLikes], 'likes')
+	console.log(mostLikes)
+
+	return {
+		author: authorMostLikes,
+		likes: mostLikes
+	}
+}
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes
+}
+
