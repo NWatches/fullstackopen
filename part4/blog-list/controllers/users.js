163cd688e84bbe99793bca6842c2d376d9743f73
Nemr Nahle <nemrnahle@Nemrs-Air-2.lan>
2023-12-07 01:21:09 -0800

    added restrictions for usernames and passwords

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -6,6 +6,23 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
 	const { username, name, password } = request.body
 
+	if (!(username & password)) {
+		return response.status(400).json({ error: 'username or password missing'})
+	}
+
+	if (username.length < 3) {
+		return response.status(400).json({ error: 'username must be atleast 3 characters long'})
+	}
+
+	if (password.length < 3) {
+		return response.status(400).json({ error: 'password must be atleast 3 characters long'})
+	}
+
+	const existingUser = await User.findOne({ username })
+	if (existingUser) {
+		return response.status(400).json({ error: 'username must be unique'})	
+	}
+
 	const saltRounds = 10
 	const passwordHash = await bcrypt.hash(password, saltRounds)
 
